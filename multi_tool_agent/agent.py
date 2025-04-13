810f73ae6d19e92b0294ab313855b43b6d6863ae
lunakicks <100943241+christymanthara@users.noreply.github.com>
2025-04-13 07:28:13 +0200

    test to fix the tool problem

--- multi_tool_agent/agent.py
+++ multi_tool_agent/agent.py
@@ -2,18 +2,17 @@ import datetime
 from zoneinfo import ZoneInfo
 from google.adk.agents import Agent
 from dotenv import load_dotenv
-from google.adk.tools import google_search
+from google.adk.tools import google_search, agent_tool
 from google.adk.agents import LlmAgent, BaseAgent
-
-
 
 
 description_agent = LlmAgent(
     model="gemini-2.0-flash-exp", # Required: Specify the LLM 
     name="question_answer_agent", # Required: Unique agent name
     description="A helpful assistant agent that can answer questions. It should give more information to the children about the object they are intersted in. Make sure that you keep the responses short and simple. "
-                "You can also use the google search tool to find more information about the object. ",
+                "You can also use the google_search tool to find more information about the object. ",
     instruction="""Respond to the query using google search""",
+    # google_search is a pre-built tool which allows the agent to perform Google searches.
     tools=[google_search], # Provide an instance of the tool
 )
 
@@ -60,13 +59,15 @@ root_agent = Agent(
     instruction=(
         "I can answer your questions in a concise and fun way to make it easy for children to understand. "
     ),
-    tools=[get_weather, get_current_time],
+    # tools=[a,b],
+    
     sub_agents=[ # Assign sub_agents here
         greeting_agent,
         farewell_agent,
         description_agent,
         model_task_agent,
     ],
+    tools=[agent_tool.AgentTool(agent=description_agent)],
 
 )
 
