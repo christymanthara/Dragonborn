e15d399e84f28dff617d1838f02c0e2e91c4091d
lunakicks <100943241+christymanthara@users.noreply.github.com>
2025-04-13 06:50:14 +0200

    Cleaned unused functions

--- multi_tool_agent/agent.py
+++ multi_tool_agent/agent.py
@@ -7,60 +7,10 @@ from google.adk.agents import LlmAgent, BaseAgent
 
 
 
-def get_weather(city: str) -> dict:
-    """Retrieves the current weather report for a specified city.
-
-    Args:
-        city (str): The name of the city for which to retrieve the weather report.
-
-    Returns:
-        dict: status and result or error msg.
-    """
-    if city.lower() == "new york":
-        return {
-            "status": "success",
-            "report": (
-                "The weather in New York is sunny with a temperature of 25 degrees"
-                " Celsius (41 degrees Fahrenheit)."
-            ),
-        }
-    else:
-        return {
-            "status": "error",
-            "error_message": f"Weather information for '{city}' is not available.",
-        }
-
-
-def get_current_time(city: str) -> dict:
-    """Returns the current time in a specified city.
-
-    Args:
-        city (str): The name of the city for which to retrieve the current time.
-
-    Returns:
-        dict: status and result or error msg.
-    """
-
-    if city.lower() == "new york":
-        tz_identifier = "America/New_York"
-    else:
-        return {
-            "status": "error",
-            "error_message": (
-                f"Sorry, I don't have timezone information for {city}."
-            ),
-        }
-
-    tz = ZoneInfo(tz_identifier)
-    now = datetime.datetime.now(tz)
-    report = (
-        f'The current time in {city} is {now.strftime("%Y-%m-%d %H:%M:%S %Z%z")}'
-    )
-    return {"status": "success", "report": report}
 
 description_agent = LlmAgent(
     model="gemini-2.0-flash-exp", # Required: Specify the LLM 
-    name="question_answer_agent", # Requdired: Unique agent name
+    name="question_answer_agent", # Required: Unique agent name
     description="A helpful assistant agent that can answer questions. It should give more information to the children about the object they are intersted in. Make sure that you keep the responses short and simple. "
                 "You can also use the google search tool to find more information about the object. ",
     instruction="""Respond to the query using google search""",
@@ -102,13 +52,13 @@ model_task_agent = BaseAgent(name="TaskExecutor")
 # )
 
 root_agent = Agent(
-    name="weather_time_agent",
+    name="Coordinator_agent",
     model="gemini-2.0-flash-exp",
     description=(
-        "Agent to answer questions about the time and weather in a city and also flower information using Plant.id API."
-    ),
+        "Agent to coordinate the entire working of the system. Remember that you are dealing with children less than 8 years old. So be polite and friendly."
+     ),
     instruction=(
-        "I can answer your questions about the time and weather in a city. Also more information about flowers using Plant.id API. "
+        "I can answer your questions in a concise and fun way to make it easy for children to understand. "
     ),
     tools=[get_weather, get_current_time],
     sub_agents=[ # Assign sub_agents here
